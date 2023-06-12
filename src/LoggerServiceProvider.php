80213cd17620458575eedbf0593682f50a1ee02e
abbas <abbasmomeny1994@gmail.com>
2023-06-12 12:12:43 +0330

    feat: implement models() to retrive all models

--- src/LoggerServiceProvider.php
+++ src/LoggerServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace Celysium\Logger;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 
 class LoggerServiceProvider extends ServiceProvider
@@ -13,5 +19,33 @@ class LoggerServiceProvider extends ServiceProvider
 
     public function register()
     {
+        $models = $this->models();
+
     }
+
+
+    public function models()
+    {
+        $app_path = app_path();
+        $real_path = realpath($app_path) . DIRECTORY_SEPARATOR;
+        $namespace = app()->getNamespace();
+
+        return collect((new Finder)->in($app_path)->files()->name('*.php'))
+            ->map(function (SplFileInfo $model) use ($real_path, $namespace) {
+                return $namespace . str_replace(['/', '.php'], ['\\', ''], $model->getRelativePathname());
+
+            })
+            ->filter(function ($model) {
+                return class_exists($model) && $this->isModel($model);
+            })
+            ->values();
+    }
+
+    protected function isModel(string $model): bool
+    {
+        $uses = class_parents($model);
+        return in_array(Model::class, $uses) && !in_array(Pivot::class, $uses);
+    }
+
+
 }

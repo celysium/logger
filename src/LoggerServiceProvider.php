f81954d5389c98e8302ae605be75ca8c6dcc7e0d
abbas <abbasmomeny1994@gmail.com>
2023-06-12 09:47:10 +0330

    feat: add ServiceProvider

---
+++ src/LoggerServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Celysium\Logger;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class LoggerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}

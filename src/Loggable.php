5f0080b726e740c244a92ec346a5e6282bf204a0
Abbas Momeni <a.momeni@zoot.com>
2023-11-14 11:50:11 +0330

    add log() to trait Loggable

--- src/Loggable.php
+++ src/Loggable.php
@@ -2,6 +2,7 @@
 
 namespace Celysium\Logger;
 
+use Celysium\Authenticate\Facades\Authenticate;
 use Celysium\Logger\Models\ModelLog;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
@@ -12,5 +13,19 @@ trait Loggable
     {
         /** @var Model $this */
         return $this->morphMany(ModelLog::class, 'model');
+    }
+
+    public function log(string $type): void
+    {
+        /** @var Model $this */
+        ModelLog::query()->create([
+            'user_id'      => Authenticate::id(),
+            'user_name'    => Authenticate::name(),
+            'service_name' => env('APP_NAME'),
+            'model_id'     => $this->getKey(),
+            'model_type'   => get_class($this),
+            'type'         => $type,
+            'attributes'   => $this->toArray()
+        ]);
     }
 }

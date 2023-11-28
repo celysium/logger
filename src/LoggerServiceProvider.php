eb2214c1fa34ad5a7b128bc232e7a74be03fd65c
Abbas Momeni <a.momeni@zoot.com>
2023-11-28 16:59:28 +0330

    add: loggable macro to http add: bind listener for http

--- src/LoggerServiceProvider.php
+++ src/LoggerServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace Celysium\Logger;
 
+use Celysium\Logger\Listeners\ConnectionFailedListener;
+use Celysium\Logger\Listeners\ResponseReceivedListener;
 use Celysium\Logger\Middlewares\LogRequest;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class LoggerServiceProvider extends ServiceProvider
@@ -15,5 +19,14 @@ class LoggerServiceProvider extends ServiceProvider
         /** @var Router $router */
         $router = app('router');
         $router->pushMiddlewareToGroup('api', LogRequest::class);
+
+
+        Http::macro('loggable', function (string $name) {
+            Http::withHeaders(['REQUEST-LOG-NAME' => $name, 'REQUEST-STARTED' => microtime(true)]);
+        });
+
+        Event::listen('Illuminate\Http\Client\Events\ResponseReceived', ResponseReceivedListener::class);
+        Event::listen('Illuminate\Http\Client\Events\ConnectionFailed', ConnectionFailedListener::class);
+
     }
 }
